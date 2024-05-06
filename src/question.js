deb6dc29fc8df30ceb05f161bbca9d904d799ad1
Yves Martinez <yves.oliver.m@gmail.com>
2024-05-06 17:12:15 -0400

    Day 1 Done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,15 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
-    // 2. shuffleChoices()
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            let randomNum = Math.floor(Math.random() * (i + 1));
+            [this.choices[i], this.choices[randomNum]] = [this.choices[randomNum], this.choices[i]];
+        }
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+     constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            let randomNum = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[randomNum]] = [this.questions[randomNum], this.questions[i]];
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+          if (answer === answer) {
+            this.correctAnswers++;
+          }
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+            if (this.currentQuestionIndex < this.questions.length) {
+                return false;
+            }
+            else {
+                return true;
+            }
+    }
 }
